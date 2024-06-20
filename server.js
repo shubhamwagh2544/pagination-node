a399d92b079d2e8ca3b8ac1f894732b99ebb1fd6
shubham.wagh <shubham.wagh@globant.com>
2024-06-20 10:18:31 +0530

    websocket conn added

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { WebSocketServer, WebSocket } from 'ws'
 import { posts, users } from './data.js'
 import paginationResult from './paginationmiddleware.js'
 import connectAndSeedDatabase from './db.js'
@@ -20,6 +21,28 @@ app.get('/posts', paginationResult(posts), (req, res) => {
 })
 
 // server start
-app.listen(3000, function handler() {
+const server = app.listen(3000, function handler() {
     console.log('server started on 3000')
+})
+
+// create websocket server
+const wss = new WebSocketServer({ server: server })
+
+// connection event
+wss.on('connection', function (ws) {
+    console.log('new client connected to ws server')
+    ws.on('message', function (message) {
+        console.log('message received %s', message)
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                ws.send(message, { binary: false })
+            }
+        })
+    })
+    ws.on('close', function () {
+        console.log('client disconnected')
+    })
+    ws.on('error', (error) => {
+        console.error(`WebSocket error: ${error}`);
+    });
 })
